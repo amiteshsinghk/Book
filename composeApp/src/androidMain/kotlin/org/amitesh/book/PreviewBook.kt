92bf0d83f6b086525c41fb55577624b0a50d0884
Amitesh Singh <amiteshsinghk@gmail.com>
2024-11-21 15:47:37 +0530

    Book list screen

---
+++ composeApp/src/androidMain/kotlin/org/amitesh/book/PreviewBook.kt
@@ -0,0 +1,21 @@
+package org.amitesh.book
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.amitesh.book.books.presentation.book_list.components.BookSearchBar
+
+@Preview(backgroundColor = 0xFFFF, showBackground = true)
+@Composable
+fun BookSearchBarPreview() {
+    MaterialTheme {
+        BookSearchBar(
+            searchQuery = "Kotlin",
+            onSearchQueryChange = {},
+            onImeSearch = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
