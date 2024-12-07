a717d35c266e8371b9cb8aaea9939e49ec6a2f0c
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-07 23:57:31 +0530

    SelectedViewModel.kt

---
+++ composeApp/src/commonMain/kotlin/org/amitesh/book/books/data/network/RemoteBookDataSource.kt
@@ -0,0 +1,14 @@
+package org.amitesh.book.books.data.network
+
+import org.amitesh.book.books.data.dto.SearchResponseDto
+import org.amitesh.book.core.domain.DataError
+import org.amitesh.book.core.domain.Result
+
+// presentation -> domain <- data
+// we don't have to call data layer from presentation layer it will violate the principal of clean architecture.
+interface RemoteBookDataSource {
+    suspend fun searchBooks(
+        query: String,
+        resultLimit:Int? = null
+    ): Result<SearchResponseDto, DataError.Remote>
+}
