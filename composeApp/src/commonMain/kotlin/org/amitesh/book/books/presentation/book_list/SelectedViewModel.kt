fef446c451fc6040c1bbd211ced449b4d58dc2b5
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-07 23:56:55 +0530

    SelectedViewModel.kt

---
+++ composeApp/src/commonMain/kotlin/org/amitesh/book/books/presentation/book_list/SelectedViewModel.kt
@@ -0,0 +1,15 @@
+package org.amitesh.book.books.presentation.book_list
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.amitesh.book.books.domain.Book
+
+class SelectedViewModel: ViewModel() {
+    private val _selectedBook = MutableStateFlow<Book?>(null)
+    val selected = _selectedBook.asStateFlow()
+
+    fun onSelectedBook(book: Book?){
+        _selectedBook.value = book
+    }
+}
