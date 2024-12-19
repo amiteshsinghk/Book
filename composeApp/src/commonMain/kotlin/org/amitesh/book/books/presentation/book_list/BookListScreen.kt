f1f5be576908855aa46fcf62d96610f0af4ffb2d
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-20 00:49:32 +0530

    update.

--- composeApp/src/commonMain/kotlin/org/amitesh/book/books/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/org/amitesh/book/books/presentation/book_list/BookListScreen.kt
@@ -3,10 +3,8 @@ package org.amitesh.book.books.presentation.book_list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
