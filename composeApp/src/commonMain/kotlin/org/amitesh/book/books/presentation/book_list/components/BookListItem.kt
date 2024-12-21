1b473b991d28bf8f17065e644343a02eb4b31368
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-21 11:34:42 +0530

    update.

--- composeApp/src/commonMain/kotlin/org/amitesh/book/books/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/org/amitesh/book/books/presentation/book_list/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package org.amitesh.book.books.presentation.book_list.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -19,7 +21,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -29,19 +30,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import book.composeapp.generated.resources.Res
 import book.composeapp.generated.resources.ic_error
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import org.amitesh.book.books.domain.Book
 import org.amitesh.book.core.presentation.LightBlue
+import org.amitesh.book.core.presentation.PulseAnimation
 import org.amitesh.book.core.presentation.SandYellow
 import org.jetbrains.compose.resources.painterResource
 
@@ -87,8 +91,17 @@ fun BookListItem(
                         imageLoad = Result.failure(it.result.throwable)
                     }
                 )
+
+                val painterState by painter.state.collectAsStateWithLifecycle()
+                val transition  by animateFloatAsState(
+                    targetValue = if(painterState is AsyncImagePainter.State.Success) 1f else 0f,
+                    animationSpec = tween(durationMillis = 800)
+                )
+
                 when (val result = imageLoad) {
-                    null -> CircularProgressIndicator()
+                    null -> PulseAnimation(
+                            modifier = Modifier.size(60.dp)
+                        )
                     else -> {
                         Image(
                             painter = if (result.isSuccess) painter
@@ -100,6 +113,12 @@ fun BookListItem(
                                 0.65f,
                                 matchHeightConstraintsFirst = true // This will try to fit the height with in the Image frame.
                             )
+                                .graphicsLayer {
+                                    rotationX = (1f - transition) * 30f
+                                    val scale = 0.8f + (0.2f * transition)
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
                         )
                     }
 
